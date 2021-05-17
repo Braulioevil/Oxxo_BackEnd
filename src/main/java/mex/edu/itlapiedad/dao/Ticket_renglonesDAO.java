544e917f06f5298a72263a02b8a3b16c1c5e2ddb
Gabriel <gabramgu@gmail.com>
2021-05-16 21:47:13 -0500

    tickets renglones

---
+++ src/main/java/mex/edu/itlapiedad/dao/Ticket_renglonesDAO.java
@@ -0,0 +1,19 @@
+package mex.edu.itlapiedad.dao;
+
+import java.util.List;
+
+import mx.edu.itlapiedad.models.Ticket_renglones;
+
+public interface Ticket_renglonesDAO {
+
+	Ticket_renglones buscar(int id);
+
+	Ticket_renglones insertar(Ticket_renglones ticket_renglones);
+
+	List<Ticket_renglones> consultarTicket_renglones();
+
+	void actualizar(Ticket_renglones ticket_renglones);
+
+	void eliminar(int id);
+
+}
